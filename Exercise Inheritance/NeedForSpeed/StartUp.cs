0767401cc9ec0fd06ab21265def5bdffd204e7ac
ANGEL <atabakov99@abv.bg>
2022-07-02 12:44:34 +0300

    Exercise Inheritance

--- Exercise Inheritance/NeedForSpeed/StartUp.cs
+++ Exercise Inheritance/NeedForSpeed/StartUp.cs
@@ -6,7 +6,9 @@ namespace NeedForSpeed
     {
         static void Main(string[] args)
         {
-            
+            CrossMotorcycle sportCar = new CrossMotorcycle(100, 100);
+            sportCar.Drive(9);
+            Console.WriteLine(sportCar.Fuel);
         }
     }
 }
--- Exercise Inheritance/NeedForSpeed/Vehicle.cs
+++ Exercise Inheritance/NeedForSpeed/Vehicle.cs
@@ -6,6 +6,22 @@ namespace NeedForSpeed
 {
     public class Vehicle
     {
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+        public const double DefaultFuelConsumption = 1.25;
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
 
+        public virtual void Drive(double kilometers)
+        {
+            if (this.Fuel - kilometers * this.FuelConsumption >= 0)
+            {
+                this.Fuel = this.Fuel - kilometers * this.FuelConsumption;
+            }
+        }
     }
 }

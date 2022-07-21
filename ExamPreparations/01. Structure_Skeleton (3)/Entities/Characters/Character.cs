516e3b274e754e0900bc4ad67c7bf10d543497b0
ANGEL <atabakov99@abv.bg>
2022-07-21 22:37:54 +0300

    Warcroft

---
+++ ExamPreparations/01. Structure_Skeleton (3)/Entities/Characters/Character.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Reflection.Metadata;
+using WarCroft.Constants;
+using WarCroft.Entities.Inventory;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+        private string name;
+
+        private float baseHealth;
+
+        private float health;
+        private float baseArmor;
+        private float armor;
+
+        
+        public float Armor
+        {
+            get => armor;
+           private set
+            {
+                if (value < 0)
+                {
+                    return;
+                } 
+                armor = value;
+            }
+        }
+
+        public float BaseArmor
+        {
+            get => baseArmor;
+            set => baseArmor = value;
+        }
+
+        public float Health 
+        {
+            get => health;
+            set
+            {
+                if (value > BaseHealth || value < 0)
+                {
+                    return;
+                }
+                health = value;
+            }
+        }
+
+        public float BaseHealth
+        {
+            get => baseHealth;
+            set => baseHealth = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.CharacterNameInvalid);
+                }
+                this.name = value;
+            }
+        }
+
+        public Bag Bag { get; private set; }
+
+        public float AbilityPoints { get; private set; } 
+
+        public bool IsAlive { get; set; } = true;
+
+		protected void EnsureAlive()
+		{
+			if (!this.IsAlive)
+			{
+				throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+			}
+		}
+        public void TakeDamage(float hitPoints)
+        {
+            if (IsAlive != true)
+            {
+                return;
+            }
+            else
+            {
+                float leftHitPoints = 0;
+                if (hitPoints > armor)
+                {//   100     120 
+                    leftHitPoints = Math.Abs(armor - hitPoints);
+                    armor -= hitPoints;
+
+                    health -= leftHitPoints;
+                }
+            }
+        }
+        public void UseItem(Item item)
+        {
+           EnsureAlive();
+           item.AffectCharacter(this);
+        }
+
+	}
+}
---
+++ ExamPreparations/01. Structure_Skeleton (3)/Entities/Inventory/Bag.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using WarCroft.Constants;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Inventory
+{
+    public abstract class Bag : IBag
+    {
+        private readonly List<Item> items;
+
+        
+        private int capacity = 100;
+
+        protected Bag(int capacity)
+        {
+            Capacity = capacity;
+            this.items = new List<Item>();
+        }
+        public int Capacity
+        {
+            get { return capacity;}
+            set { value = capacity; }
+        }
+
+        public int Load => Items.Select(x=> x.Weight).Sum();
+        public IReadOnlyCollection<Item> Items => items;
+        public void AddItem(Item item)
+        {
+            if (Load + item.Weight > Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.ExceedMaximumBagCapacity);
+            }
+            this.items.Add(item);
+        }
+
+        public Item GetItem(string name)
+        {
+            if (!items.Any())
+            {
+                throw new InvalidOperationException(ExceptionMessages.EmptyBag);
+            }
+            var findItem = items.FirstOrDefault(x => x.GetType().Name == name);
+            if (findItem is null)
+            {
+                throw  new ArgumentException(string.Format(ExceptionMessages.ItemNotFoundInBag, name));
+            }
+
+            items.Remove(findItem);
+            return findItem;
+        }
+    }
+}
